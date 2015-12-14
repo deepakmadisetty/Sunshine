a4c07edd29a5b2feb5670fa04b0d959eae6b1a12
Deepak Madisetty <deepak.vy400@gmail.com>
2015-12-13 21:34:42 -0600

    Fixed the bug in Share Intent

--- app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -38,6 +39,8 @@ public class DetailActivityFragment extends Fragment {
 
         if(mShareActionProvider != null) {
             mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d(LOG_TAG, "mShareActionProvider is null!");
         }
     }
 
